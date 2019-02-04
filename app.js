efbbc23a502e35efb56230a6525c387feca419be
ADJAROO OGAGA <ogagaadjaro@gmail.com>
2019-02-04 02:12:20 -0800

    make it good

---
+++ app.js
@@ -0,0 +1,32 @@
+var express= require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+
+    res.render("home");
+});
+
+app.get("/salespage1", function(req, res){
+    
+    res.render("salespage1");
+
+});
+app.get("/salespage2", function(req, res){
+    
+    res.render("salespage2");
+
+});
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("server has started");
+});
